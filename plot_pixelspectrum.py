405d8db67867b2632c07334504e427d2ba959f63
Markus Gruber <gruber@physik.uni-bonn.de>
2024-11-26 11:56:54 +0100

    [pixelspectrum] ENH: add script for plotting pixel spectra

---
+++ plot_pixelspectrum.py
@@ -0,0 +1,66 @@
+import numpy as np
+from tqdm import tqdm
+import os
+import h5py
+import argparse
+
+import matplotlib.pyplot as plt
+
+# Create a histogram based on a array of hits per event and save it
+# to the folder `direc` with the name `filename`.
+def pixelspectrum(hits, direc, filename):
+    plt.clf()
+    maximum = hits.max()
+    plt.hist(hits, bins=int(maximum*1.1)+1, range=(0, maximum*1.1))
+    plt.xlabel("Active pixels per event")
+    plt.ylabel("Number of events")
+    plt.grid(True)
+    plt.savefig(direc + '/' + str(filename) + '.pdf')
+
+def main():
+    # Get the arguments
+    parser = argparse.ArgumentParser(description='Plot the pixelspectra (active pixels per event)')
+    parser.add_argument('runpath', type=str, help='Path to the hdf5 file or a folder containing multiple hdf5 files')
+    args = parser.parse_args()
+
+    run = args.runpath
+    # Ploting if one file is provided
+    if run.endswith('h5'):
+        # Create the folder for storing the plot
+        direc = os.path.dirname(run) + "/Pixelspectrum"
+        if os.path.exists(direc) == False:
+            os.makedirs(direc)
+        datafile = os.path.basename(run)
+        f = h5py.File(run, 'r+')
+        filename = datafile.replace('.h5', '')
+        timepix_version = f['reconstruction'].attrs['TimepixVersion'][0].decode('utf-8')
+        reconstruction = f['reconstruction']
+        for name in reconstruction:
+            hits = f.get('reconstruction/' + name + '/chip_0/hits')[:]
+            pixelspectrum(hits, direc, filename)
+    # Plotting if a folder with files is provided
+    elif os.path.isdir(run):
+        print("Plotting the spectra for all hdf5 files in the folder")
+
+        # Create the folder for storing the plots
+        direc = os.path.dirname(run) + "/Pixelspectrum"
+        if os.path.exists(direc) == False:
+            os.makedirs(direc)
+
+        # Iterate over all hdf5 files in the folder
+        files = [file for file in os.listdir(run) if file.endswith('.h5')]
+        for file in tqdm(files):
+            datei_pfad = os.path.join(run, file)
+            with h5py.File(datei_pfad, 'r') as f:
+                filename = file.replace('.h5', '')
+                timepix_version = f['reconstruction'].attrs['TimepixVersion'][0].decode('utf-8')
+                reconstruction = f['reconstruction']
+                for name in reconstruction:
+                    hits = f.get('reconstruction/' + name + '/chip_0/hits')[:]
+                    pixelspectrum(hits, direc, filename)
+    else:
+        print("Please choose a correct data file or folder")    
+
+
+if __name__ == "__main__":
+    main()
--- readme.md
+++ readme.md
@@ -29,3 +29,11 @@ The `runpath` is a path to an Hdf5 file. Additionally there are the following op
 - `events`Select how many events are plotted starting with the first event. Default is 100 events. 0 for all events.
 - `event_offset` Selects the first event that is plotted (Counting starts at 0). Default is 0.
 - `plot3d` Add two additional plots in xz and yz projection. Only works with Timepix3 data.
+
+### Pixelspectrum
+Plots a histogram of the number of active pixels per event. It can be used as follows:
+```
+python3 plot_pixelspectrum.py [-h] runpath
+```
+The `runpath` can either be the full path to a hdf5 file but also to a folder contatingin
+hdf5 files. In the latter case a plot for each hdf5 file is generated.
